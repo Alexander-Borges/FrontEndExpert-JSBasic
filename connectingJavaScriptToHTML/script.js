8ad0431f32e4fd58f614def2b1bf520a04bcf78e
Alexander Borges <borges2721@gmail.com>
2024-05-14 23:04:35 -0400

    Add files via upload

---
+++ connectingJavaScriptToHTML/script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button");
+button.addEventListener("click", setbackgroundColor);
+
+function setbackgroundColor(){
+    document.body.style.backgroundColor = "#00334C";
+}
